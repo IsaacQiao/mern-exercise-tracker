7c83808b714f163baf60fa766962918a2b40e4e3
IsaacQiao <qiaobm@hotmail.com>
2021-05-01 16:39:19 -0700

    feat: add routes for updateding exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,37 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error:' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    const exerciseId = req.params.id;
+
+    Exercise.findById(exerciseId)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error:' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    const exerciseId = req.params.id;
+
+    Exercise.findByIdAndDelete(exerciseId)
+        .then(() => res.json(`Exercise: ${exerciseId} is deleted!`))
+        .catch(err => res.status(400).json('Error:' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    const exerciseId = req.params.id;
+
+    Exercise.findById(exerciseId)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error:' + err));
+        })
+        .catch(err => res.status(400).json('Error:' + err));
+});
+
 module.exports = router;
